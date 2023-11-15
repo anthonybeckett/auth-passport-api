95925d7f1a37f05d6e5c88fa59d92cc236c5b265
Anthony Beckett <antneyb10@gmail.com>
2023-11-15 20:11:33 +0000

    Installed passport and started a very basic readme

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function register(Request $request): void
+    {
+        //
+    }
+
+    public function login(Request $request): void
+    {
+        //
+    }
+
+    public function profile(): void
+    {
+        //
+    }
+
+    public function logout(Request $request): void
+    {
+        //
+    }
+}
